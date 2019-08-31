ffd39c77f2d0ce8496f4394e917f611f5f90216d
BhushanBhawsar <bhushanbhawsar@gmail.com>
2019-08-31 19:16:20 +0530

    non transacted code

---
+++ camel-transacted/src/main/java/com/example/demo/routes/NonTransactedRouteBuilder.java
@@ -0,0 +1,20 @@
+package com.example.demo.routes;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class NonTransactedRouteBuilder extends RouteBuilder {
+
+	@Override
+	public void configure() throws Exception {
+
+		from("jms:queue:incoming").process(i -> {
+			String msg = i.getIn().getBody(String.class);
+			i.getOut().setBody(msg + " : Outgoing");
+			;
+		}).to("file:C:\\");
+
+	}
+
+}
